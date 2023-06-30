44d2381dca25c21e9d611fca92ca25cdf711d566
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-06-30 14:37:53 -0500

    working on getting it up and running

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = 3001
-const notes = (/public/notes.html) 
+const fs = require('fs')
+// const notes = (/public/notes.html) 
 
 app.use(express.static('public'));
 
-app.get('/', (req,res ) => res.sendFile();
+app.get('/', (req,res ) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get('/api')
+
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+
+});
+
+// app.get('/api/notes', (req, res) => {
 
 
 
