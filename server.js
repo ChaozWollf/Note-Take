9df24d0ebe50fbdd6394ffe40049d27b8858ad68
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-01 11:44:15 -0500

    getting routes working

--- server.js
+++ server.js
@@ -6,24 +6,35 @@ const fs = require('fs')
 
 app.use(express.static('public'));
 
-app.get('/', (req,res ) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+
+
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('/api/', (req, res) => {
-
+app.get('/api', (req, res) => {
 fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
      res.json(JSON.parse(data));
 });
 });
 
-// app.get('/api/notes', (req, res) => {
+app.get('api/notes',(req,res) => {
+    res.json()
+});
 
+app.post('api/notes',(req,res) => {
+    res.json()
+});
+
+app.delete('api/notes', (req, res) => {
+    res.json()
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 
 app.listen(PORT, () => 
