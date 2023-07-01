6900766c627dc74f1c15ae5e5f88f8a147c4f553
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-01 16:04:03 -0500

    attempted to fix syntax in routes folder

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const fs = require('fs')
 
 app.get('/api', (req, res) => {
 
@@ -15,20 +15,32 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
-//save notes
 app.post('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        res.json(JSON.parse(data));
-        res.json.push(notes, data);
-        fs.writeFile('./db.db.json', notes, (err))
-        if (err) throw err;
-        res.json(JSON.stringify(notes))
-
-        res.json(notes)
-
+        let notes = JSON.parse(data);
+        notes.push(req.body);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(notes);
+        });
     });
 });
+
+//save notes
+// app.post('/api/notes', (req, res) => {
+//     fs.readFile('./db/db.json', (err, data) => {
+//         if (err) throw err;
+//         res.json(JSON.parse(data));
+//         res.json.push(notes, data);
+//         fs.writeFile('./db.db.json', notes, (err))
+//         if (err) throw err;
+//         res.json(JSON.stringify(notes))
+
+//         res.json(notes)
+
+//     });
+// });
 
 
 //delete notes
