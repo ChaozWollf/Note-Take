bb67af095d89a177b20f542675fae1d8c8d900a6
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-01 20:30:17 -0500

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 // const fs = require('fs')
 // const notes = require('./db/db.json')
 const routes = require ('./routes/routes.js')
