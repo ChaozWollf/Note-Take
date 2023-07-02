83b97ea37cfa5485ccc56fd0aabce4a9c06a9d54
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-01 20:47:19 -0500

    notes in html added

---
+++ routes/routess.js
@@ -0,0 +1,11 @@
+app.get('/notes', (req, res) => {
+    res.send(
+        `<p>${notes.html}</p>`
+    );
+});
+
+app.get('/notes', (req, res) => {
+    res.send(
+        `<p>${index.html}</p>`
+    );
+});
